09abcc26b4908627b8036207c9c895c7c2f00b43
Antonio Oliveira <antonioeduardo1998@gmail.com>
2021-11-14 23:27:52 -0300

    commit files

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from 'express';
+
+import { router } from "./routes";
+
+const app = express();
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query;
+    return response.json(code);
+})
+
+app.listen(4000, () => console.log('Listening on port 4000'));
