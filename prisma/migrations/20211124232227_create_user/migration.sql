9f376e0d76b2b57609dda9520df54eeac0610e4a
Antonio Eduardo <antonioeduardo1998@gmail.com>
2021-11-24 21:08:05 -0300

    dockerfile config

---
+++ prisma/migrations/20211124232227_create_user/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "github_id" INTEGER NOT NULL,
+    "avatar_url" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
